4c7a4733adea35ea94dba58e039ffbb5027e905c
Jeanlegalzinho166 <160247798+Jeanlegalzinho166@users.noreply.github.com>
2024-10-31 11:00:22 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function criaCartao (categoria, pergunta, resposta) {
+
+}
+function criaCartao(categoria, pergunta, resposta) {
+    let container 
+}
+function criaCartao(categoria, pergunta, resposta) {
+    let container 
+}
